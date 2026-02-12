955ebea0b09b3a228ca46e4193537c9575ea82db
Marco Gizoni <devmarcogizoni@gmail.com>
2026-02-12 15:07:36 -0300

    Adiciona função para desenhar quadrado sólido
    Implementa a função desenhaQuadrado para exibir um quadrado sólido.

---
+++ Exercicios_Revisao_APC/desenhaquadrado.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*3 - Escreva uma função desenhaQuadrado que exibe um quadrado
+sólido (o mesmo número de linhas e colunas). O caracter utilizado
+para preencher o quadrado e o valor do lado são passados como
+argumentos para a função. Por exemplo, se o caracter for x e o valor
+do lado for 5, a função deverá exibir
+xxxxx
+xxxxx
+xxxxx
+xxxxx
+xxxxx*/
+
+
