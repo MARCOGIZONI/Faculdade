665b4bc5a6a0636fa281eed4bfeb8afe00f154ab
Marco Gizoni <devmarcogizoni@gmail.com>
2026-02-12 15:15:25 -0300

    Add number guessing game implementation
    Implement a number guessing game that prompts the user to guess a secret number between 0 and 1000, providing feedback on their guesses.

---
+++ Exercicios_Revisao_APC/jogo_advinhar_numero.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*7 – Crie um programa que simule um jogo de adivinhar um número
+da seguinte maneira:
+Apresente a seguinte mensagem ao usuário:
+Tenho um número entre 0 e 1000, você consegue adivinhar qual é?
+O usuário digita o primeiro palpite e o programa exibe uma das
+seguintes respostas:
+- Número muito baixo, tente novamente...
+- Número muito alto, tente novamente...
+- Parabéns, número encontrado!!!
+O programa deve ser repetido até que o usuário encontre o número
+“secreto”.
+OBS: para esse exercício utilize as instruções sobre a geração de
+números aleatórios em C.... (PESQUISEM A FUNÇÃO RAND)*/
+
+
