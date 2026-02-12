6385ce03af1a885ca6e873abe296df8ab0fafc6b
Marco Gizoni <devmarcogizoni@gmail.com>
2026-02-12 15:01:15 -0300

    Add comment block for asterisk line function
    Add a comment block for the asterisk line application.

--- Exercicios_Revisao_APC/linhas_asteristicos.c
+++ Exercicios_Revisao_APC/linhas_asteristicos.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
+/*1 - Fazer um aplicativo que produza a saída a seguir, usando uma
+função linha para produzir a linha de asteriscos.
+********************
+NOME 01
+********************
+NOME 02
+*********************/
+
