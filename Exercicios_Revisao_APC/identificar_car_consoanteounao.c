eef9c3ff509c97bb8be0643105fefe17ca053ecb
Marco Gizoni <devmarcogizoni@gmail.com>
2026-02-12 15:13:56 -0300

    Implement consonant identification function
    Add function to check if a character is a consonant.

---
+++ Exercicios_Revisao_APC/identificar_car_consoanteounao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*6 - Fazer uma função que receba um caractere como parâmetro e
+retorne verdadeiro caso o caractere seja uma consoante, e falso caso
+contrário. (Pesquisar valor dos caracteres na Tabela ASC)*/
+
+
