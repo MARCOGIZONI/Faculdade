78b39281643287115cc7bc9baa6fe8dfd31d0c58
Marco Gizoni <devmarcogizoni@gmail.com>
2026-02-12 15:11:13 -0300

    Add sphere volume calculation application
    Implement function to calculate the volume of a sphere based on user input for radius.

---
+++ Exercicios_Revisao_APC/calc_vol_v_esfera.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*4 - Escreva um aplicativo para solicitar ao usuário o raio r de uma
+esfera, e calcular o volume V da esfera usando uma função, e exibir
+o resultado. Utilize a seguinte fórmula para determinar o volume: V = 4sobre3 pi relevadoa3*/
+
+
