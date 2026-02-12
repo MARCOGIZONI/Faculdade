bf7b7656236ea80677350d132283713a126988f0
Marco Gizoni <devmarcogizoni@gmail.com>
2026-02-12 15:02:37 -0300

    Create linhas_caracteres_desejados.c with initial code
    Implement initial structure for desired character line output application.

---
+++ Exercicios_Revisao_APC/linhas_caracteres_desejados.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/*2- Fazer um aplicativo que produza a saída a seguir, usando uma
+função linha para produzir a linha de caracteres desejada.
+********************
+NOME 01
+====================
+NOME 02
+####################*/
+
+
